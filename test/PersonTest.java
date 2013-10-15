216c4d412a0119c09646e4ac0f62f3b1bfc03ae0
T Bentham <t.bentham@warwick.ac.uk>
2013-10-15 19:23:05 +0000

    PersonTest.java - adjusting calls to Person.java, due to privatisation

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -6,7 +6,7 @@ public class PersonTest {
     @Test
     public void ConstructorAddsCoordinates() throws Exception {
         Person person = new Person(1.0, 2.0);
-        assertTrue(person.xPos == 1.0);
-        assertTrue(person.yPos == 2.0);
+        assertTrue(person.getPosX() == 1.0);
+        assertTrue(person.getPosY() == 2.0);
     }
 }
