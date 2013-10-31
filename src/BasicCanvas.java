49b669921ff443c9d415e1cfa674d465a40d1770
csujen <marcus@ruination.org.uk>
2013-10-31 14:33:53 +0000

    Added reverse edges because we are using directed algorithm

--- src/BasicCanvas.java
+++ src/BasicCanvas.java
@@ -7,23 +7,21 @@ public class BasicCanvas {
     public static void main(String[] args) throws Exception {
         double d = System.currentTimeMillis();
 
-        World world = new World(50);
-        world.addWall(1, 1, 40, 1);
-        world.addWall(40, 1, 40, 40);
-        world.addWall(40, 40, 1, 40);
-        world.addWall(1, 40, 1, 1);
+        World world = new World(3);
+        world.addWall(1, 1, 1, 2);
 
         world.setUp();
         world.printFloorPlan();
-        world.computeDijsktraTowards(3, 10);
-        LinkedList<Vertex> path = world.getPath(20, 20).getVertices();
+        world.computeDijsktraTowards(0, 2);
+        Thread.sleep(5);
+        LinkedList<Vertex> path = world.getPath(0, 1).getVertices();
         for (Vertex v : path) {
             System.out.println(v);
         }
         System.out.println();
         System.out.println("Subgoals:");
 
-        LinkedList<Vertex> path2 = world.getPath(20, 20).getSubGoals();
+        LinkedList<Vertex> path2 = world.getPath(0, 1).getSubGoals();
         for (Vertex v : path2) {
             System.out.println(v);
         }
