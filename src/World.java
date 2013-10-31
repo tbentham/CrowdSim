ecdf69d20ae7817b697a1ebc9154e2df70683110
csujen <marcus@ruination.org.uk>
2013-10-31 13:45:03 +0000

    Refactored World to saner API

--- src/World.java
+++ src/World.java
@@ -16,6 +16,8 @@ public class World {
     private List<Edge> edges;
     private Vertex[][] nodeArray;
     private DijkstraAlgorithm dijkstra;
+    private boolean setUp;
+    private boolean routesComputed;
 
     public World(int sideLength) {
         this.sideLength = sideLength;
@@ -24,13 +26,25 @@ public class World {
         nodes = new ArrayList<Vertex>();
         edges = new ArrayList<Edge>();
         nodeArray = new Vertex[sideLength][sideLength];
+        setUp = false;
+        routesComputed = false;
     }
 
     public void addWall(double x1, double y1, double x2, double y2) {
+        setUp = false;
+        routesComputed = false;
         walls.add(new Wall(x1, y1, x2, y2));
     }
 
-    public void populateFloorPlan() {
+    public void setUp() {
+        populateFloorPlan();
+        populateVertexArray();
+        createEdges();
+        setUp = true;
+        routesComputed = false;
+    }
+
+    private void populateFloorPlan() {
         for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
                 floorPlan[i][j] = 0;
@@ -45,7 +59,10 @@ public class World {
         }
     }
 
-    public void printFloorPlan() {
+    public void printFloorPlan() throws WorldNotSetUpException {
+        if (!setUp) {
+            throw new WorldNotSetUpException("printFloorPlan called before setting up world");
+        }
         for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
                 System.out.print(floorPlan[i][j]);
@@ -56,7 +73,7 @@ public class World {
         }
     }
 
-    public void populateVertexArray() {
+    private void populateVertexArray() {
         for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
                 nodeArray[i][j] = null;
@@ -69,7 +86,7 @@ public class World {
         }
     }
 
-    public void createEdges() {
+    private void createEdges() {
         for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
                 if (floorPlan[i][j] == 0) {
@@ -101,13 +118,21 @@ public class World {
         }
     }
 
-    public void setGoal(int x, int y) {
+    public void computeDijsktraTowards(int x, int y) throws WorldNotSetUpException {
+        if (!setUp) {
+            throw new WorldNotSetUpException("computerDijsktraTowards called before setting up world");
+        }
         Graph graph = new Graph(nodes, edges);
         dijkstra = new DijkstraAlgorithm(graph);
         dijkstra.execute(nodeArray[x][y]);
+        routesComputed = true;
     }
 
-    public Path getPath(int x, int y) {
+
+    public Path getPath(int x, int y) throws RoutesNotComputedException {
+        if (!routesComputed) {
+            throw new RoutesNotComputedException("getPath called before routes were computed");
+        }
         return new Path(dijkstra.getPath(nodeArray[x][y]));
     }
 
