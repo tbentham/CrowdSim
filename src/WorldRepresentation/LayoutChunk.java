4041226091dc15f15d262bf3604f9972ceafaad0
Luke <luke@luke-K53E.(none)>
2014-02-03 15:38:05 +0000

    Chunk communication

--- src/WorldRepresentation/LayoutChunk.java
+++ src/WorldRepresentation/LayoutChunk.java
@@ -3,8 +3,11 @@ package WorldRepresentation;
 import javax.vecmath.Point2d;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Queue;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class LayoutChunk implements Runnable {
 
@@ -18,8 +21,11 @@ public class LayoutChunk implements Runnable {
     boolean finished;
     private CyclicBarrier barrier;
     private int steps;
-    private int barriers;
-
+    LayoutChunk[][] chunks;
+    public LinkedBlockingQueue<Person> q;
+//    private HashMap<Point2d, Queue<Person>> queues;
+//    private Queue<Person> newPeople;
+    
     public LayoutChunk(double leftXBoundary, double rightXBoundary, double topYBoundary, double bottomYBoundary, ArrayList<Wall> walls, CyclicBarrier barrier, int steps) {
         people = new ArrayList<Person>();
         this.topYBoundary = topYBoundary;
@@ -31,8 +37,7 @@ public class LayoutChunk implements Runnable {
         finished = false;
         this.barrier = barrier;
         this.steps = steps;
-        this.barriers = 0;
-        
+        q = new LinkedBlockingQueue<Person>();
     }
 
     public void addWall(double x1, double y1, double x2, double y2) {
@@ -87,18 +92,47 @@ public class LayoutChunk implements Runnable {
         }
         return num;
     }
+    
+    public void addChunks(LayoutChunk[][] chunks) {
+    	this.chunks = chunks;
+    }
+    
+    public void putPerson(Person p) {
+    	try {
+			this.q.put(p);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private void addPeople() {
+    	while(!q.isEmpty()){
+    		this.people.add(q.poll());
+    	}
+    }
+    
 
     public void run() {
         for (int i = 0; i < this.steps; i++) {
-            for (Person p : people) {
+        	
+        	System.out.println("My queue has: " + q.size() + " And I have: " + people.size());
+        	addPeople();
+        	ArrayList<Person> toRemove = new ArrayList<Person>();
+        	for (Person p : people) {
                 try {
                     p.advance(gWalls, people, 0.25);
+                    if(!isPointInside(p.getLocation().x, p.getLocation().y)){
+                    	int xIndex = (int) p.getLocation().x / 50;
+                    	int yIndex = (int) p.getLocation().y / 50;
+                    	chunks[xIndex][yIndex].putPerson(p);
+                    	toRemove.add(p);
+                    }
                 }
                 catch (Exception e) {
                     //
-                }
+                } 
             }
-            
+        	people.removeAll(toRemove);
             try {
 				barrier.await();
 			} catch (InterruptedException | BrokenBarrierException e) {
