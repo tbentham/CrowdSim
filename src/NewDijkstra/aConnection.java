8026cbf1c47e2794d9646c05c25cfd330693bd6c
Marcus <marcus@ruination.org.uk>
2014-03-13 17:46:27 +0000

    first stage in adding queue

---
+++ src/NewDijkstra/aConnection.java
@@ -0,0 +1,11 @@
+package NewDijkstra;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marcus
+ * Date: 13/03/2014
+ * Time: 17:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class aConnection {
+}
